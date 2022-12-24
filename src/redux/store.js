7adee6bfd2e0edf213a88474f155f6c69405c536
Saroj Shrestha <codkanxa@gmail.com>
2022-12-24 16:01:57 +0545

    hi

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,17 +5,26 @@ import MenuReducer from "./menu-items/menu-items.reducer";
 import storage from "redux-persist/lib/storage";
 import { persistReducer, persistStore } from "redux-persist";
 import cartToolkit from "./cart/cart.toolkit";
+
 const persistConfig = {
   key: "root",
   storage,
 };
+
 const rootReducer = combineReducers({ menu: MenuReducer, cart: cartToolkit });
 const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const middlewares = [
+  // Add other middlewares here if you have any
+];
+
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
+
 const store = configureStore({
   reducer: persistedReducer,
-
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({ serializableCheck: false }).concat(logger),
+  middleware: middlewares,
 });
 
 export default store;
